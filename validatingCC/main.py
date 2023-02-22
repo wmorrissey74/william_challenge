49d688ce00e25beadd3b1438a7b39075c8970814
William Morrissey <bmorrissey@Williams-MBP.attlocal.net>
2023-02-22 15:00:10 -0600

    adding validatingCC.

---
+++ validatingCC/main.py
@@ -0,0 +1,19 @@
+
+# https://www.hackerrank.com/challenges/validating-credit-card-number/problem
+import re
+
+
+sample = open('sample.txt', 'r')
+creditCardNumbers = sample.readlines()[1:]
+firstDigitInNumber = ('4', '5', '6')
+
+for creditCardNumber in creditCardNumbers:
+    if creditCardNumber.strip().startswith(firstDigitInNumber):
+        if re.match(r'(\d{4}-){3}\d{4}', creditCardNumber.strip()):
+            print("valid ")
+        elif re.match(r'\d{16}', creditCardNumber.strip()):
+            print("valid ")
+        else:
+            print("invalid ")
+    else:
+        print("invalid")
